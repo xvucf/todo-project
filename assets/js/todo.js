dc148441331ac43b2a056f0e7fe6f3ee8da93375
xvucf <31140569+xvucf@users.noreply.github.com>
2018-03-30 18:43:52 +0200

    update todo.js
    adding coments

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -3,18 +3,23 @@ $("ul").on( "click", "li", function(){
     $(this).toggleClass("completed");
 })
 $("ul").on("click", "span" , function(){
+    //li fades out
     $(this).parent().fadeOut(500, function(){
     $(this).remove();
 }                            )
+    //stop looping
 event.stopPropagation();
 });
 
 //creating a new todo 
 $("input[type='text']").keypress(function(event){
+    //check if its enter (13)
     if(event.which === 13){
+       //add text val to new todo
         var todoText = $(this).val();
-       $(this).val("");
         $("ul").append("<li><span><i class=\"far fa-trash-alt n\"></i></span> " + todoText + "</li>");
+        //clear input
+        $(this).val("");
     }
 })
 $(".fa-plus").click(function(){
