7ff30382a73dcba01546a9ae0be2ab4cda6271e3
xvucf <31140569+xvucf@users.noreply.github.com>
2018-03-30 18:30:01 +0200

    Add files via upload

---
+++ assets/js/todo.js
@@ -0,0 +1,23 @@
+//alert("CONNECTED!");
+$("ul").on( "click", "li", function(){
+    $(this).toggleClass("completed");
+})
+$("ul").on("click", "span" , function(){
+    $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+}                            )
+event.stopPropagation();
+});
+
+//creating a new todo 
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+       $(this).val("");
+        $("ul").append("<li><span><i class=\"far fa-trash-alt n\"></i></span> " + todoText + "</li>");
+    }
+})
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+                
